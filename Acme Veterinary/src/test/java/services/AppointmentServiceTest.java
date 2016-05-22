5de0be64e39aef7ec75796561383f10dd732308e
Claudio Manuel Vega Bernal <clavegber@users.noreply.github.com>
2016-05-22 19:11:01 +0000

    Nuevos Tests de Gestionar mascotas, cambiar información personal

--- Acme Veterinary/src/test/java/services/AppointmentServiceTest.java
+++ Acme Veterinary/src/test/java/services/AppointmentServiceTest.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -28,57 +29,77 @@ public class AppointmentServiceTest extends AbstractTest {
 
 	@Autowired
 	private VeterinaryService veterinaryService;
-	
+
 	@Autowired
 	private AppointmentService appointmentService;
-	@Autowired 
+	
+	@Autowired
 	private PetService petService;
-	
 
-	// Tests -------------------------------------------------	
-	
-	//Comprobamos que un usuario puede pedir cita para un veterinario
-	
+	// Tests -------------------------------------------------
+
+	// Comprobamos que un usuario puede pedir cita para un veterinario
+
 	@Test
-	public void testCreateAppointment(){
-		
-		//Nos logueamos como customer1
+	public void testCreateAppointment() {
+
+		// Nos logueamos como customer1
 		authenticate("customer1");
-		
+
 		Collection<Appointment> appointments;
 		appointments = appointmentService.findByPrincipalNoExpired();
-		
+
 		Veterinary veterinary;
 		Pet pet = petService.findOne(57);
 		veterinary = veterinaryService.findOne(49);
-		Appointment appointment  = appointmentService.create();
+		Appointment appointment = appointmentService.create();
 		appointment.setVeterinary(veterinary);
-		appointment.setDay(new Date(12/13/2016));
+		appointment.setDay(new Date(12 / 13 / 2016));
 		appointment.setEndTime("21:00:00");
 		appointment.setPet(pet);
 		appointment.setReason("TEST");
 		appointment.setStartTime("20:00:00");
-		appointment.setMoment(new Date(03/03/2016));
 		appointmentService.save(appointment);
-		
+
 		Assert.isTrue(appointments.contains(appointment));
-		
+
 		unauthenticate();
-		
+
 	}
 
-	//Comprobamos que un usuario puede cancelar una cita 
-	
+	// Comprobamos que un usuario puede cancelar una cita
+
 	@Test
-	public void testCancelAppointment(){
-		authenticate( "customer1");
-		
+	public void testCancelAppointment() {
+		authenticate("customer1");
+
 		appointmentService.cancelAppointment(67);
-		
+
 		unauthenticate();
 	}
 	
 
+	// Test Negativos ----------------------------------------
 
+	// Comprobamos que un usuario no puede cancelar una sita que no es suya
+	
+	//TODO pasa el test y no debería, ERROR
+	@Test()
+	@Rollback(true)
+	public void testCancelAppointmentNegative() {
+
+		authenticate("customer1");
+
+		Appointment appointment;
+		
+		appointment = appointmentService.findOne(65);
+
+		appointmentService.cancelAppointment(appointment.getId());
+
+		unauthenticate();
+
+	}
+	
+	
 
 }
